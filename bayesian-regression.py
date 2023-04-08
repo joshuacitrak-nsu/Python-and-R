982b474991226dac88ee7a2db2319b084bc5bc64
Matt Rosinski <mattrosinski+1@gmail.com>
2023-04-08 17:00:17 +1000

    Plotting prediction distributions

--- bayesian-regression.py
+++ bayesian-regression.py
@@ -1,5 +1,8 @@
 import stan
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 import nest_asyncio
 nest_asyncio.apply()
 
@@ -30,6 +33,7 @@ model {
   // Likelihood
   popularity ~ normal(mu, sigma);
 }
+
 """
 
 # Prepare the data for Stan
@@ -41,7 +45,27 @@ data = {
 
 # Compile and run the Stan model
 posterior = stan.build(stan_code, data=data, random_seed=1)
-fit = posterior.sample(num_chains=4, num_samples=1000)
+fit = posterior.sample(num_chains=5, num_samples=1000)
+
+callable_methods = [
+    method
+    for method in dir(fit)
+    if (
+        callable(getattr(fit, method))
+        and not method.startswith('__')
+        and not method.startswith('_')
+    )
+]
+
+print(callable_methods)
+
+fit.to_frame()
+
+# Get posteriors of predicted scores for each observation
+y_rep = fit["predicted_popularity"]
+
+# The first 10 rows from each chain (default is 4 chains)
+y_rep[:10, :5]
 
 # Extract parameter samples
 alpha_samples = fit['alpha'].flatten()
@@ -66,18 +90,15 @@ probability = within_range.mean()
 
 print("Probability that beta is within the range:", probability)
 
-import matplotlib.pyplot as plt
-import numpy as np
-
 # Prepare data for plotting
-num_chains = 4
+num_chains = 5
 iterations = 1000
 song_age_samples = beta_samples.reshape((num_chains, iterations))
 
 # Create a custom color cycle for the plot
 colors = plt.cm.viridis(np.linspace(0, 1, num_chains))
 
-# Create the plot
+# Create the plot of the chain iterations
 fig, ax = plt.subplots()
 for i, chain in enumerate(song_age_samples):
     ax.plot(np.arange(1, iterations + 1), chain, color=colors[i], label=f"Chain {i + 1}", linewidth=0.6)
@@ -92,4 +113,86 @@ ax.spines['right'].set_visible(False)
 ax.spines['left'].set_linewidth(0.5)
 ax.spines['bottom'].set_linewidth(0.5)
 
+plt.show()
+
+
+# Prepare the new data and update the data dictionary ----
+predict_data = {'song_age': [500, 9000]}
+N_new = len(predict_data['song_age'])
+
+# Add N_new and song_age_new to the data dictionary
+data.update({'N_new': N_new, 'song_age_new': predict_data['song_age']})
+
+# Run the sampler with the updated data
+posterior = stan.build(stan_code, data=data, random_seed=1)
+
+fit = posterior.sample(num_chains=5, num_samples=1000)
+
+predicted_popularity = fit["predicted_popularity"]
+
+# Predictions for popularity with song age of 500 days
+summary_500 = np.percentile(predicted_popularity[:, 0], [2.5, 25, 50, 75, 97.5])
+
+# Predictions for popularity with song age of 9000 days
+summary_9000 = np.percentile(predicted_popularity[:, 1], [2.5, 25, 50, 75, 97.5])
+
+print("Summary for popularity with song age of 500 days:")
+print(summary_500)
+
+print("Summary for popularity with song age of 9000 days:")
+print(summary_9000)
+
+predicted_popularity.shape
+predicted_popularity_reshaped = predicted_popularity.reshape(193, 5000).T
+
+predictions_df = pd.DataFrame(predicted_popularity_reshaped, columns=["500 days", "9000 days"])
+
+
+# Define new song ages
+new_song_ages = np.array([500, 9000])
+
+# Compute predicted popularity for new song ages using posterior samples
+predicted_popularity = np.outer(alpha_samples, np.ones_like(new_song_ages)) + \
+  np.outer(beta_samples, new_song_ages)
+
+# Create a DataFrame for predicted popularity
+predictions_df = pd.DataFrame(predicted_popularity, columns=["500 days", "9000 days"])
+
+# Visualise the distribution using seaborn
+predictions_melted = predictions_df.melt(var_name="song_age", value_name="predict")
+
+sns.set_style("whitegrid")
+fig, (ax1, ax2) = plt.subplots(nrows=2, ncols=1, figsize=(8, 8), sharex=True)
+
+# Plot density for 500 days
+sns.kdeplot(
+    data=predictions_melted[predictions_melted["song_age"] == "500 days"],
+    x="predict",
+    fill=True,
+    common_norm=False,
+    alpha=0.8,
+    linewidth=0,
+    ax=ax1,
+)
+ax1.set_title("500 days")
+ax1.set_xlabel("")  # remove x-label for the top subplot
+ax1.set_ylabel("Density")
+
+# Plot density for 9000 days
+sns.kdeplot(
+    data=predictions_melted[predictions_melted["song_age"] == "9000 days"],
+    x="predict",
+    fill=True,
+    common_norm=False,
+    alpha=0.8,
+    linewidth=0,
+    ax=ax2,
+)
+ax2.set_title("9000 days")
+ax2.set_xlabel("Song popularity on Spotify")
+ax2.set_ylabel("Density")
+
+fig.suptitle("Predicted Radiohead song popularity distributions as a function of song age")
+sns.despine()
+plt.tight_layout()
 plt.show()
